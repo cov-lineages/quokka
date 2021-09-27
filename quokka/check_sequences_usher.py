1474234f169efad6c54eb6844040719b1128d571
user <user@zooadmins-MBP.broadband>
2021-09-27 12:34:13 +0100

    Updated check_sequences_usher.py to check all sequences against a lineage

--- quokka/check_sequences_usher.py
+++ quokka/check_sequences_usher.py
@@ -1,7 +1,9 @@
-#Extracts the sequences from the UShER tree in a clade of interest and checks whether samples within a given list are within this clade
+#Extracts the sequences from the UShER tree in a clade of interest and checks whether sequences designated to a given lineage are outside the
+#lineage clade in the UShER tree and whether sequences designated outside the lineage are within the lineage clade in the UShER tree
 #Used to check whether sequences designated to a lineage are in the lineage's clade in the UShER tree
 #Takes a sample-paths file, created from a protobuf file using matUtils: matUtils extract -i tree.pb -S sample-paths
-#Prints sample names that are in the designations for a lineage but are not in the lineage in the UShER tree
+#Outputs 2 files: cluster_outside.txt containing sequences designated to the lineage that don't cluster within the lineage and cluster_inside.txt containing
+#sequences designated outside the lineage that cluster within the lineage
 
 import argparse
 import re
@@ -11,8 +13,9 @@ if __name__ == "__main__":
     parser.add_argument("-s", help = "sample-paths containing sequences and their paths in the tree")
     parser.add_argument("-n", nargs = "+", help = "Names of sequences to examine")
     parser.add_argument("-m", help = "Mutation to define the root node of the clade")
-    parser.add_argument("-d", help = "Sequence designations to be checked")
-    parser.add_argument("-o", help = "File to which sequences in clade of interest will be written")
+    parser.add_argument("-d", help = "lineages.csv containing current sequence designations")
+    parser.add_argument("-l", help = "Lineage of interest, sequences designated to this lineage will be checked")
+    parser.add_argument("-o", help = "Prefix of output files")
     args = parser.parse_args()
 
     #Sequence names
@@ -21,7 +24,11 @@ if __name__ == "__main__":
     #Mutation of interest
     mutation = args.m
 
-    outFile = open(args.o, "w")
+    #Lineage of interest
+    lineage = args.l
+
+    outFileOutside = open(args.o + "_cluster_outside.txt", "w")
+    outFileInside = open(args.o + "_cluster_inside.txt", "w")
 
     #Nodes with the mutation of interest leading to the samples of interest
     nodes = list()
@@ -43,18 +50,21 @@ if __name__ == "__main__":
         print("Mutation " + mutation + " occurs on different branches in the provided samples")
     else:
         node = nodes[0]
-        #Iterate through the sequences, check if they contain the node in their path and add to the list of sequences if so
+        #Iterate through the sequences, check if they contain the node in their path and add to the list of sequences in the clade if so
         with open(args.s) as fileobject:
             for line in fileobject:
                 if node + ":" in line.strip():
-                    sN = re.sub("\\|.*", "", line.strip().split("\t")[0])
-                    clade.append(sN)
-                    outFile.write(sN)
+                    clade.append(re.sub("\\|.*", "", line.strip().split("\t")[0]))
     
-    #Iterate through the designated sequences, check if they are in the clade and print if not
+    #Iterate through the designated sequences, check if they are designated to the lineage of interest and not in the clade or designated to
+    #another lineage and in the clade
     with open(args.d) as fileobject:
+        next(fileobject)
         for line in fileobject:
-            if line.strip() not in clade:
-                print(line.strip())
+            if (line.strip().split(",")[1] == lineage) and (line.strip().split(",")[0] not in clade):
+                outFileOutside.write(line.strip().split(",")[0] + "\n")
+            if (line.strip().split(",")[1] != lineage) and (line.strip().split(",")[0] in clade):
+                outFileInside.write(line.strip().split(",")[0] + "\n")
     
-    outFile.close()
+    outFileOutside.close()
+    outFileInside.close()
